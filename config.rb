9f3941a81f0b6d8fe1e9afbf3c8cc7a1b4b70147
Sisi Wei <me@sisiwei.com>
2014-04-11 21:24:49 -0400

    More setup for s3 sync.

--- config.rb
+++ config.rb
@@ -69,4 +69,9 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  activate :s3_sync do |s3_sync|
+    s3_sync.bucket                     = 'games.sisiwei.com' # The name of the S3 bucket you are targetting. This is globally unique.
+    s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
+  end
 end
